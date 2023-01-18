6acd648fddc9a3b756bfa6d055bdf7f2fb7ebfee
utsav <guptautsavkumar126@gmail.com>
2023-01-18 22:51:56 +0530

    seven

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,13 +4,7 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        if(isVeg==true){
-            setPrice(450);
-            setTotal(450);
-        }
-        else{
-            setPrice(600);
-            setTotal(600);
-        }
+        addExtraCheese();
+        addExtraToppings();
     }
 }
